c8fe2cba185b7b23990322f2fcabea8b84b1ed43
SugarJonnyy <220040079+SugarJonnyy@users.noreply.github.com>
2025-12-26 10:55:01 +0000

    learn basic comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,11 @@
+console.log(undefined == 0);
+console.log(null >= 0);
+console.log(null > 0);
+console.log(undefined == null);
+console.log(undefined === null);
+console.log("2" == 2);
+ 
+
+
+
+/* these type of comparisons are largely avoided for better code and readibilty */
