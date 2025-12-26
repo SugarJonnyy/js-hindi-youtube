caa7e4af9289d3e4647bebd050ff8be0635b6278
SugarJonnyy <220040079+SugarJonnyy@users.noreply.github.com>
2025-12-26 10:24:33 +0000

    conversion of data types introduction

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+
+// few conversion examples 
+
+// string to boolean
+let numberInString = "-1000"
+let convertTo = Boolean(numberInString)
+console.log(convertTo)
+
+
+// null to boolean
+let nullValue = null
+let convertNull = Boolean(nullValue)
+console.log(convertNull);
+
+
+// undefined to boolean 
+let undefinedValue = undefined
+let convertUndefined = Boolean(undefinedValue)
+console.log(convertUndefined);
+ 
