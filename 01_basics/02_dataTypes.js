e9121a020702f46ca3c02edeeafb14bd5c759fd2
SugarJonnyy <220040079+SugarJonnyy@users.noreply.github.com>
2025-12-26 09:54:21 +0000

    learn and code data types

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,3 +2,24 @@
 
 // alert(3+3) // we are using node js not web browser
 
+let value = 88
+let isLoggedIn = true;
+let name =  "Saswata Das"
+
+/*
+Number => 2 to the pwer 53 
+BigInt => big numbers than Number data type
+String => ""
+null => a standalone value
+boolean => true or false
+undefined => not defined value 
+symbol => uniqueness 
+*/
+ 
+
+//object is one data type 
+// null is treated as object in js and undefined treated as undefined 
+
+console.log(typeof null);
+console.log(typeof undefined);
+
