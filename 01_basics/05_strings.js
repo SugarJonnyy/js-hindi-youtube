3b25ef088a15ac13dcac5c808a8f84ffccdcdc08
SugarJonnyy <220040079+SugarJonnyy@users.noreply.github.com>
2025-12-27 14:02:40 +0000

    learn string and its methods

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,24 @@
+// string and methods 
+let name = "Saswata Das"
+let year = 20
+console.log(`My name is ${name} and I am ${year} years old`);
+
+
+const gameName = new String('snake Game')
+console.log(gameName[0]);
+
+console.log(gameName.slice(0, 4));
+console.log(gameName.charAt(4));
+console.log(gameName.toUpperCase());
+console.log(gameName.indexOf('k'));
+console.log(gameName.slice(-8, 4));
+
+
+
+let newString = "     Saswata       "
+console.log(newString.trim());
+
+
+
+
+
