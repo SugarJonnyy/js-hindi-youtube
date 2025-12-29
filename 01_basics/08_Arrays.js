ed132cebf7954ea33ae1775f7e248b3a08509cd4
SugarJonnyy <220040079+SugarJonnyy@users.noreply.github.com>
2025-12-29 13:48:18 +0000

    learn Arrays and its methods

---
+++ 01_basics/08_Arrays.js
@@ -0,0 +1,19 @@
+// Arrays
+
+let myArr = ["hello", 1, 2, 3, 4, true]
+console.log(myArr);
+
+// Methods
+
+console.log(myArr.push(100));
+console.log(myArr.pop());
+console.log(myArr.join());
+console.log(myArr.slice(0,4));
+console.log(myArr);
+console.log(myArr.splice(0, 4));
+console.log(myArr);
+
+
+
+
+
