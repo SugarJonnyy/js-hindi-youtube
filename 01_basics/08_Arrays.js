a4fb1ba177998b4173e7806ec6677e28ad7a88d9
SugarJonnyy <220040079+SugarJonnyy@users.noreply.github.com>
2025-12-30 03:12:48 +0000

    learn some advance array methods

--- 01_basics/08_Arrays.js
+++ 01_basics/08_Arrays.js
@@ -14,6 +14,35 @@ console.log(myArr.splice(0, 4));
 console.log(myArr);
 
 
+// concat
+
+let anotherArrys = ["hello", "hi", "bye"]
+let anotherArrys1 = ["dont", "bro"]
+
+// let concattedArray = [...anotherArrys, ...anotherArrys1] // preferable method 
+let concattedArray = anotherArrys.concat(anotherArrys1) 
+console.log(concattedArray); 
+
+// flat method 
+
+let testArray = [1,2,3,[9,10,[7,8]],100]
+console.log(testArray.flat(3)); //depth number is 3 
+
+
+// Array conversion from any data type 
+console.log(Array.from("Saswata"));
+
+// method for making array for bunch of variables
+
+let Array1 = 200
+let Array2= 300
+let Array3 = 400
+
+console.log(Array.of(Array1,Array2,Array3));
+
+
+
+
 
 
 
